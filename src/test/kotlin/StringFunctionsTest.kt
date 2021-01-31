14c250be8bddd0126e4555182db8fe29a0b16518
Ibi <jurinka.ibi@gmail.com>
2021-01-31 10:13:06 +0100

    fix tests

--- src/test/kotlin/StringFunctionsTest.kt
+++ src/test/kotlin/StringFunctionsTest.kt
@@ -1,24 +1,30 @@
-import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
 
-class StringFunctionsTest : DescribeSpec({
-    describe("convertTimeTo24Format") {
-        val stringFunctions = StringFunctions()
-        it("should add 12h to PM if PM < 12") {
-            val result = stringFunctions.convertTimeTo24Format("01:00:01PM")
-            result shouldBe "13:00:01"
-        }
-        it("should add keep it the same if PM == 12") {
-            val result = stringFunctions.convertTimeTo24Format("12:00:01PM")
-            result shouldBe "12:00:01"
-        }
-        it("should keep it the same if AM < 12") {
-            val result = stringFunctions.convertTimeTo24Format("01:00:01AM")
-            result shouldBe "01:00:01"
-        }
-        it("should add substrack 12h if AM == 12") {
-            val result = stringFunctions.convertTimeTo24Format("12:00:01AM")
-            result shouldBe "00:00:01"
-        }
+class StringFunctionsTest {
+    val stringFunctions = StringFunctions()
+
+    @Test
+    fun `should add 12h to PM if PM smaller than 12`() {
+        val result = stringFunctions.convertTimeTo24Format("01:00:01 PM")
+        result shouldBe "13:00:01"
     }
-})
+
+    @Test
+    fun `should add keep it the same if PM == 12`() {
+        val result = stringFunctions.convertTimeTo24Format("12:00:01 PM")
+        result shouldBe "12:00:01"
+    }
+
+    @Test
+    fun `should keep it the same if AM smaller than 12`() {
+        val result = stringFunctions.convertTimeTo24Format("01:00:01 AM")
+        result shouldBe "01:00:01"
+    }
+
+    @Test
+    fun `should add substrack 12h if AM == 12`() {
+        val result = stringFunctions.convertTimeTo24Format("12:00:01 AM")
+        result shouldBe "00:00:01"
+    }
+}
